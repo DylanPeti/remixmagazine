ca6dcec4f0c351ecc421302680078968d308ba53
Dylan <dylanpetiinfo@gmail.com>
2016-02-17 11:35:23 +1300

    Latest article styling

--- wp-content/themes/remix/inc/latest.php
+++ wp-content/themes/remix/inc/latest.php
@@ -20,15 +20,15 @@ $recent_post = wp_get_recent_posts( $recent_posts, OBJECT);
 <section id="latest-article-section" class="">
 
 <div class="container">
-
 <?php $latest_categories = get_the_category($recent_post[0]->ID); ?>
+<?php $cat_class = strtolower(preg_replace("/[^A-Za-z0-9 ]/", '', $latest_categories[0]->name)); ?>
  <article class="article-latest remix-img" style="background-image: url(<?php echo remix_thumbnail_url('', 'post', $recent_post[0]->ID) ?>)">
 
         <span class="article-latest-tag">THE LATEST FROM REMIX</span>
         <div class="article-latest-exerpt">
-        <span class="article-tag"><?php echo $latest_categories[0]->name; ?></span>
+        <span class="article-tag <?php echo $cat_class; ?>"><?php echo $latest_categories[0]->name; ?></span>
             <div class="article-latest-content">
-               <h4><?php echo $recent_post[0]->post_title; ?></h4>
+               <h2><?php echo $recent_post[0]->post_title; ?></h2>
         	   <?php  echo wp_trim_words($recent_post[0]->post_excerpt, 30); ?>
         	</div>
              
@@ -37,7 +37,7 @@ $recent_post = wp_get_recent_posts( $recent_posts, OBJECT);
                <li class="entypo-facebook"></li>
                <li class="entypo-twitter"></li>
               </ul>
-              
+
         </div>
    
  </article>
