69fd337b32d82c80bb4455435f7cb45bd903a2cb
Dylan <dylanpetiinfo@gmail.com>
2016-02-19 12:04:08 +1300

    Add Remix plugin

---
+++ wp-content/plugins/remix/remix.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Remix
+Plugin URI:  http://#
+Description: Remix functionality
+Version:     1.5
+Author:      Dylan Peti
+Author URI:  http://#
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: Remix
+*/
+defined( 'ABSsPATH' ) or die( 'No script kiddies please!' );
+?>
