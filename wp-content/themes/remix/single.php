c83c16b047ca94ee8812e20e66cb71d1bd6dcb79
Carl Thompson <carl@gomomentum.marketing>
2016-02-19 16:49:59 +1300

    updates

--- wp-content/themes/remix/single.php
+++ wp-content/themes/remix/single.php
@@ -1,29 +1,27 @@
 <?php
 
 get_header(); 
-
+global $post
 ?>
 
 <div class="article-hero">
-	<h2>Article Title Here</h2>
+	<h2><?php echo $post->post_title; ?></h2>
 </div>
 <div class="container">
 	<div class="article_single">
 	
 	<div class="toolbar">
 		<span class="article-tag">Category</span>
-		<span class="author">Author Here</span>
-		<span class="date">Date Here</span>
+		<span class="author"><?php echo remix_post_author($post->ID); ?></span>
+		<span class="date"><?php echo $post->post_date; ?></span>
 		<span class="crumbs">Bread / Crumbs</span>
 	</div>
 	  <div class="row article_content">
-	   <div class="col-md-8">
-	   		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lobortis, sapien sed ultrices venenatis, orci neque dictum arcu, sit amet eleifend eros ipsum vel nulla. Suspendisse ac consectetur risus. Vivamus viverra est at condimentum porta. Phasellus sem velit, fermentum et neque nec, laoreet finibus odio. Phasellus semper tristique augue, ut consequat elit imperdiet sed. Suspendisse ut lorem nulla. Nam ultrices interdum felis at convallis. Proin maximus, diam eu malesuada euismod, lacus nisl hendrerit est, efficitur sagittis augue orci eu erat.</p>
 
-			<p>Morbi tempor neque quis congue viverra. Integer eget pellentesque metus, ac ultrices nisi. Nulla eget est a dui vestibulum cursus. Nullam sodales tellus eros, nec ornare nunc pretium ut. Maecenas lacinia fermentum turpis. Nullam feugiat lorem nec dolor malesuada, eu consectetur elit rutrum. Nulla ullamcorper, lorem sit amet gravida semper, metus nibh faucibus lorem, quis tristique est ex ut metus. Proin non mauris sed elit vulputate porttitor sit amet vel justo. Ut sed dolor ac libero tempor convallis. Integer eu metus mattis, ultricies nunc quis, varius lorem. Fusce et dolor luctus, consequat dui a, egestas justo. Mauris sollicitudin nisl vitae orci sagittis cursus. Integer volutpat, mauris ut interdum gravida, sem metus bibendum quam, sed porta lorem turpis mollis diam. Donec pretium diam est, quis sagittis felis maximus vel.</p>
+	   <div class="col-md-8 ">
+	   			
+		<?php echo $post->post_content; ?>
 
-			<p>Proin ultrices urna non justo pharetra mattis. Maecenas at ultrices nisi, id suscipit nunc. Nam porta magna a ex porta, vitae consequat dui lobortis. Morbi risus justo, rhoncus eu tellus in, consectetur interdum nibh. Integer aliquet nulla id mauris porttitor, eget placerat neque efficitur. Praesent magna eros, fermentum nec neque eget, placerat pulvinar elit. Praesent et sollicitudin eros. Cras tempus egestas mi eu tincidunt. In et erat finibus, pharetra sapien non, pellentesque est. Nulla aliquam felis a sapien consequat aliquam. Donec eu pulvinar ipsum, eget vehicula orci. Aenean dictum sagittis condimentum. Nulla facilisi. Sed eu auctor dui, vitae facilisis sem. Nam elementum consectetur orci a rutrum. Vivamus tincidunt ornare lacus, sed gravida turpis sodales ac.</p>	
-		
 	   </div>
 	    <div class="col-md-4 sidebar">
 
