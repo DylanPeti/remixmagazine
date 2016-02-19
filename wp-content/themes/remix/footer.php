6a938a17b4f92db7d10a2635cbad2df12dea7a84
Carl Thompson <carl@gomomentum.marketing>
2016-02-19 18:09:18 +1300

    Footer

--- wp-content/themes/remix/footer.php
+++ wp-content/themes/remix/footer.php
@@ -1,1 +1,29 @@
-<footer></footer>
+<footer>
+	<div class="row">
+		<div class="col-md-4">
+		<img src="/wp-content/themes/remix/images/logo.png" alt="Remix Magazine" width="200">
+		<p>New Zealand's Fashion Magazine with over 17 years and 82 issues, Remix Magazine is the leading online and print publication for New Zealand fashion and lifestyle.</p>
+		<p>Digital strategy by <a href="http://gomomentum.marketing" target="blank" title="Momentum Marketing">Momentum Marketing</a></p>
+		</div>
+		<div class="col-md-4">
+		FOOTER MENU AREA
+		</div>
+		<div class="col-md-4">
+			<form action="http://remix.us2.list-manage.com/subscribe/post?u=7f9be996e2c6cb5ef2d2df5fb&amp;id=acc14b56d6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+				<div class="mc-field-group">
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Address">
+				</div>
+				<div class="mc-field-group">
+					<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name">
+				</div>
+				<div id="mce-responses" class="clear">
+					<div class="response" id="mce-error-response" style="display:none"></div>
+					<div class="response" id="mce-success-response" style="display:none"></div>
+				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+				<div style="position: absolute; left: -5000px;"><input type="text" name="b_7f9be996e2c6cb5ef2d2df5fb_acc14b56d6" value=""></div>
+				<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn"></div>
+				<p class="smalltext">We hate spam too. Unsubscribe at any time.</p>
+			</form>
+		</div>
+	</div>
+</footer>
