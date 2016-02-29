0e6bc2e205945a1b8530d224bf705168bec38979
Dylan <dylanpetiinfo@gmail.com>
2016-03-01 10:53:53 +1300

    hero carousel

--- wp-content/themes/remix/front-page.php
+++ wp-content/themes/remix/front-page.php
@@ -9,27 +9,70 @@ get_header();
   	$active_items[] = get_post( $item->post_id );
   }
    
+?>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
 
+  <div class="carousel-inner" role="listbox">
+
+  
+
+<?php
+ $count = 0;
  foreach ($active_items as $dog) {
- 
+ $count++;
+ if($count == 1) {
+ 	$active = "active";
+ } else {
+ 	$active = '';
+ }
 ?>
 
-<div class="hero" style="background-image: url(<?php echo remix_thumbnail_url('', 'post', $dog->ID) ?>)">
-	<div class="container">
-		<div class="article_exerpt">
-			<span class="article-tag">Category</span>
-			<h2><?php echo $dog->post_title; ?></h2>
-			<p><strong><a href=""> READ FULL ARTICLE</strong></a></p>
-			<span class="author"><?php echo $dog->post_author; ?></span>
-		 	<ul class="entypo-icons">
-	         <li class="entypo-facebook"></li>
-	         <li class="entypo-twitter"></li>
-	        </ul>
-		</div>
-	</div> 
-</div>
+<div class="item <?php echo $active; ?>">
+    <div class="hero" style="background-image: url(<?php echo remix_thumbnail_url('', 'post', $dog->ID) ?>)">
+    	<div class="container">
+    		<div class="article_exerpt">
+    			<span class="article-tag">Category</span>
+    			<h2><?php echo $dog->post_title; ?></h2>
+    			<p><strong><a href=""> READ FULL ARTICLE</strong></a></p>
+    			<span class="author"><?php echo $dog->post_author; ?></span>
+    		 	<ul class="entypo-icons">
+    	         <li class="entypo-facebook"></li>
+    	         <li class="entypo-twitter"></li>
+    	        </ul>
+    		</div>
+    	</div> 
+    </div>
+ </div>
 
 <?php } ?>
+
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
