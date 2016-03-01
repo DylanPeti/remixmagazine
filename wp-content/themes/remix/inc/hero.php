bbb5036dd91c5a21a7cc1ccb155ce7235df8b217
Carl Thompson <carl@gomomentum.marketing>
2016-03-01 19:14:51 +1300

    left and right sliders

--- wp-content/themes/remix/inc/hero.php
+++ wp-content/themes/remix/inc/hero.php
@@ -12,14 +12,3 @@
 		</div>
 	</div> 
 </div>
-
-<!-- <section id="toolbar-section">
-<div class="toolbar">
-	<div class="container">
-	  <div class="main-search">
-		 <?php include ('wp-content/themes/remix/inc/search.html'); ?>
-	  </div>
-		<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'menu-remix')); ?>
-	</div>
-</div>
-</section> -->
