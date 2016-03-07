b58f5fa69b6244dde4eedaa97017c693722f3132
Dylan <dylanpetiinfo@gmail.com>
2016-03-08 09:49:36 +1300

    Update scss

--- wp-content/themes/remix/js/remix.js
+++ wp-content/themes/remix/js/remix.js
@@ -1,27 +1,4 @@
 jQuery( document ).ready(function($) {
-  
-  //bootsrap slider auto
-// $('#myCarousel').carousel({
-//   interval: 10000
-// })
-
-// $('.carousel .item').each(function(){
-//   var next = $(this).next();
-//   if (!next.length) {
-//     next = $(this).siblings(':first');
-//   }
-//   next.children(':first-child').clone().appendTo($(this));
-  
-//   if (next.next().length>0) {
-//     next.children(':first-child').clone().appendTo($(this));
-//     next.children(':first-child').clone().appendTo($(this));
-//     next.children(':first-child').clone().appendTo($(this));
-
-//   }
-//   else {
-//   	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-//   }
-// });
 
 
 });
