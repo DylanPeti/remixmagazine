b399e1603eddd21e021acd54e148f3d2c53d67fc
Dylan <dylanpetiinfo@gmail.com>
2016-03-10 15:04:16 +1300

    Update latest

--- wp-content/themes/remix/inc/latest.php
+++ wp-content/themes/remix/inc/latest.php
@@ -20,32 +20,39 @@ $recent_post = wp_get_recent_posts( $recent_posts, OBJECT);
 <section id="latest-article-section" class="">
 
 <div class="container">
+<?php
+$category = get_the_category($recent_post[0]->ID);
+$category = $category[0]->name;
+?>
+
 <?php $latest_categories = get_the_category($recent_post[0]->ID); ?>
 <?php $cat_class = strtolower(preg_replace("/[^A-Za-z0-9 ]/", '', $latest_categories[0]->name)); ?>
  <article class="article-latest remix-img">
-<!--  <a href="<?php echo get_the_permalink($recent_post[0]->ID); ?>"> -->
-
+    <a href="<?php echo get_the_permalink($recent_post[0]->ID); ?>"> 
         <span class="article-latest-tag">THE LATEST FROM REMIX</span>
         <div class="article-latest-img remix-img" style="background-image: url(<?php echo remix_thumbnail_url($recent_post[0]) ?>)"></div>
         <div class="article-latest-exerpt">
-        <span class="article-tag <?php echo $cat_class; ?>"><?php echo $latest_categories[0]->name; ?></span>
+        <span class="article-tag <?php echo $cat_class; ?>"><?php echo $category; ?></span>
             <div class="article-latest-content">
+         
                <h2><?php echo $recent_post[0]->post_title; ?></h2>
+               
         	   <?php  echo wp_trim_words($recent_post[0]->post_excerpt, 30); ?>
         	</div>
              
-<!--            
-            <ul class="entypo-icons">
+       
+<!--             <ul class="entypo-icons">
              <div class="social-btn" id="fbshare" data-share="<?php echo $link ?>,<?php echo $title ?>,<?php echo $image ?>">
                <li class="entypo-facebook"></li>
               </div>
               <div class="social-btn"><li class="entypo-twitter"></li></div>
             </ul>
-
  -->
+
 
         </div>
 <!--  </a>   -->
+</a>
  </article>
 
 <?php include('editors-choice.php'); ?>
