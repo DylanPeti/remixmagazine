7e3371f1df5d8ec41fa5b799b0917248962e55d3
Dylan <dylanpetiinfo@gmail.com>
2016-03-15 11:05:20 +1300

    Add editors choice post

--- wp-content/themes/remix/inc/editors-choice.php
+++ wp-content/themes/remix/inc/editors-choice.php
@@ -1,22 +1,22 @@
 <?php 
 
-$culture_cat = get_cat_ID("Culture");
+$post = get_post(9767);
 
-$category = get_category($culture_cat);
+$category = get_the_category($post->ID);
 
+$category = $category[0]->name;
 
 ?>
 
 <article class="article-editors-choice">
 
-    	<div class="article-img" style="background-image: url(<?php echo remix_thumbnail_url($category->name, 'cat') ?>)">
+    	<div class="article-img" style="background-image: url(<?php echo remix_thumbnail_url($post) ?>)">
       	</div>
 	
 		 <div class="article_exerpt">
-		 <?php $cat_class = strtolower(preg_replace("/[^A-Za-z0-9 ]/", '', $category->name)); ?>
-		 <span class="article-tag <?php echo $cat_class; ?>"><?php echo $category->name; ?></span>
-		 	<h2><?php echo remix_post_title($category->name); ?></h2>
-
+		 <?php $cat_class = strtolower(preg_replace("/[^A-Za-z0-9 ]/", '', $category)); ?>
+		 <span class="article-tag <?php echo $cat_class; ?>"><?php echo $category; ?></span>
+		 	<h2><?php echo $post->post_title; ?></h2>
 		 	<ul class="entypo-icons">
              <li class="entypo-facebook"></li>
              <li class="entypo-twitter"></li>
