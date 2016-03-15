b9771cc116058f89361544153c5602698e026767
Dylan <dylanpetiinfo@gmail.com>
2016-03-15 13:24:40 +1300

    Category posts

--- wp-content/themes/remix/category.php
+++ wp-content/themes/remix/category.php
@@ -2,8 +2,43 @@
 
 get_header(); 
 
-get_section('articles');
+$categories = get_the_category();
+$category_id = $categories[0]->cat_ID;
 
-get_footer(); 
+$args = array(
+    'numberposts' => 40,
+    'offset' => 0,
+    'category' => $category_id,
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'suppress_filters' => true 
 
+ );
 ?>
+
+
+<?php $posts = get_posts($args); ?>
+
+<section id="article-section" class="black">
+  
+  <div class="container">
+   
+    <div class="article-collection">
+     
+    <?php foreach($posts as $post) : ?>
+     
+         <?php article($post); ?>
+       
+      <?php endforeach; ?>
+      
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<?php get_footer(); ?>
