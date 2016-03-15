f42440484b6eefe91ab4953e4064f415c375da34
Dylan <dylanpetiinfo@gmail.com>
2016-03-15 14:19:50 +1300

    Update editor's choice

--- wp-content/themes/remix/inc/editors-choice.php
+++ wp-content/themes/remix/inc/editors-choice.php
@@ -11,11 +11,12 @@ $category = $category[0]->name;
 <article class="article-editors-choice">
 <a href="<?php echo get_the_permalink($post->ID); ?>">
     	<div class="article-img" style="background-image: url(<?php echo remix_thumbnail_url($post) ?>)">
+    	 <span class="article-editors-tag">EDITOR'S CHOICE</span>
       	</div>
 	
 		 <div class="article_exerpt">
 		 <?php $cat_class = strtolower(preg_replace("/[^A-Za-z0-9 ]/", '', $category)); ?>
-		 <span class="article-tag <?php echo $cat_class; ?>">Editor's Choice</span>
+		 <span class="article-tag <?php echo $cat_class; ?>"><?php echo $category; ?></span>
 		 	<h2><?php echo $post->post_title; ?></h2>
 		 	<ul class="entypo-icons">
              <li class="entypo-facebook"></li>
