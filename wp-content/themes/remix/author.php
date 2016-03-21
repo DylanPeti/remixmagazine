fa08274060074bffc2daf35e755664cb91ec839e
Dylan <dylanpetiinfo@gmail.com>
2016-03-21 13:49:41 +1300

    Add articles by author

--- wp-content/themes/remix/author.php
+++ wp-content/themes/remix/author.php
@@ -15,11 +15,40 @@ get_header();
 
  <div class="col-md-6">
 
-    <h2><?php echo get_the_author_meta('user_nicename', $author); ?></h2>
+    <h2><?php echo get_the_author_meta('display_name', $author); ?></h2>
     
     <p><?php echo get_user_meta($author, 'description')[0]; ?></p>
+
+    <?php 
+
+    $args = array( 'author' => $author, 'posts_per_page' => 5, 'status' => 'publish' );
+
+    $posts = get_posts($args);
+
+
+    ?>
  	
  </div>
+
+ <section id="article-section" class="black">
+  
+  <div class="container">
+
+
+   <div class="row">
+    <div class="col-md-12"><h2 class="author-title">Articles by <?php echo get_the_author_meta('display_name', $author); ?></h2></div>
+    </div>
+   
+    <div class="article-collection">
+
+    
+    <?php foreach($posts as $article) {
+    	article($article);
+    } ?>
+
+    </div>
+    </div>
+    </div>
 
 
 </div>
