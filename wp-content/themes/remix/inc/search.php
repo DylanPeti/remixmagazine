f3740c1c5209058f51c84287caf221d6643b57bd
Dylan <dylanpetiinfo@gmail.com>
2016-04-12 16:55:04 +1200

    Update mobile styles

---
+++ wp-content/themes/remix/inc/search.php
@@ -0,0 +1,8 @@
+<div class="main-search">
+	<form role="search" method="get" class="search-form" action="http://remixmagazine.dev/">
+			<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:" style="
+			width: 100%;
+			">
+		<input type="submit" class="search-submit" value="SEARCH">
+	</form>
+</div>
