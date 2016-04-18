22273dcc3a68a0277ed5f5e0c5d460f861bc9a82
Dylan <dylanpetiinfo@gmail.com>
2016-04-18 12:04:30 +1200

    Add search

--- wp-content/themes/remix/inc/search.php
+++ wp-content/themes/remix/inc/search.php
@@ -3,6 +3,7 @@
 			<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:" style="
 			width: 100%;
 			">
-		<input type="submit" class="search-submit" value="SEARCH">
+		<input type="submit" class="search-submit" value="">
+		<i class="fa fa-search" aria-hidden="true"></i>
 	</form>
 </div>
