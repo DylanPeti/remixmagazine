bd059cc1b86b533464a3e482e6c424001a83eec7
Dylan <dylanpetiinfo@gmail.com>
2016-04-18 12:07:52 +1200

    Update search link

--- wp-content/themes/remix/inc/search.php
+++ wp-content/themes/remix/inc/search.php
@@ -1,5 +1,5 @@
 <div class="main-search">
-	<form role="search" method="get" class="search-form" action="http://remixmagazine.dev/">
+	<form role="search" method="get" class="search-form" action="http://remixmagazine.com/">
 			<input type="search" class="search-field" placeholder="Search …" value="" name="s" title="Search for:" style="
 			width: 100%;
 			">
